d7af0707a922f98360e4227672df97a6cf1accc0
overprotective <105806513+overprotective@users.noreply.github.com>
2020-07-06 12:58:59 +0000

    debian: updated README.md

--- ConfigurationManager/ConfigSettingEntry.cs
+++ ConfigurationManager/ConfigSettingEntry.cs
@@ -39,4 +39,4 @@ namespace ConfigurationManager
         private void GetAcceptableValues(AcceptableValueBase values)
         {
             var t = values.GetType();
-            var
+            var listProp = t.GetProperty(nameof(AcceptableValu
