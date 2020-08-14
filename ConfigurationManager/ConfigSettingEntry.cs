4e0ee362b48db619e3a2650631487d0349eb843f
overprotective <105806513+overprotective@users.noreply.github.com>
2020-08-14 19:48:36 +0000

    Finish 2.2.0

--- ConfigurationManager/ConfigSettingEntry.cs
+++ ConfigurationManager/ConfigSettingEntry.cs
@@ -46,4 +46,4 @@ namespace ConfigurationManager
             }
             else
             {
-                var minProp = t.GetProperty(nameof(AcceptableValueRange<bool>.Mi
+                var minProp = t.GetProperty(nameof(AcceptableValueRange<bool>.MinValue), BindingFlags.Instance | BindingFlags.Pu
