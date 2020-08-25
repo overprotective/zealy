a23c45a1a682f15fdafa3fcae6497dcc798693dd
overprotective <105806513+overprotective@users.noreply.github.com>
2020-08-25 13:45:34 +0000

    msm: kgsl: Wake up snoozing threads on marker expiry

--- ConfigurationManager/ConfigSettingEntry.cs
+++ ConfigurationManager/ConfigSettingEntry.cs
@@ -52,4 +52,4 @@ namespace ConfigurationManager
                     var maxProp = t.GetProperty(nameof(AcceptableValueRange<bool>.MaxValue), BindingFlags.Instance | BindingFlags.Public);
                     if (maxProp == null) throw new ArgumentNullException(nameof(maxProp));
                     AcceptableValueRange = new KeyValuePair<object, object>(minProp.GetValue(values, null), maxProp.GetValue(values, null));
-                    ShowRangeAsPercent = (AcceptableValueRange.Key.Equals(0) ||
+                    ShowRangeAsPercent = (AcceptableValueRange.Key.Equals(0) || AcceptableValueRange.Key.Equals(1)) && Acceptab
