2ec9161f11eb3476952872fc3250044fb47eb85a
overprotective <105806513+overprotective@users.noreply.github.com>
2020-08-25 10:51:07 +0000

    Update Main.py

--- ConfigurationManager/ConfigSettingEntry.cs
+++ ConfigurationManager/ConfigSettingEntry.cs
@@ -52,4 +52,4 @@ namespace ConfigurationManager
                     var maxProp = t.GetProperty(nameof(AcceptableValueRange<bool>.MaxValue), BindingFlags.Instance | BindingFlags.Public);
                     if (maxProp == null) throw new ArgumentNullException(nameof(maxProp));
                     AcceptableValueRange = new KeyValuePair<object, object>(minProp.GetValue(values, null), maxProp.GetValue(values, null));
-                    ShowRangeAsPercent
+                    ShowRangeAsPercent = (AcceptableValueRange.Key.Equals(0) ||
