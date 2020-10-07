2d9b61ac96889cb7a768fb0b92f966518c1e8b05
overprotective <105806513+overprotective@users.noreply.github.com>
2020-10-07 16:43:52 +0000

    updating core

--- ConfigurationManager/ConfigurationManager.cs
+++ ConfigurationManager/ConfigurationManager.cs
@@ -10,3 +10,51 @@ using System.Linq;
 using System.Reflection;
 using BepInEx;
 using BepInEx.Logging;
+using UnityEngine;
+using BepInEx.Configuration;
+using ConfigurationManager.Utilities;
+
+namespace ConfigurationManager
+{
+    /// <summary>
+    /// An easy way to let user configure how a plugin behaves without the need to make your own GUI. The user can change any of the settings you expose, even keyboard shortcuts.
+    /// https://github.com/ManlyMarco/BepInEx.ConfigurationManager
+    /// </summary>
+    [BepInPlugin(GUID, "Configuration Manager", Version)]
+    [Browsable(false)]
+    public class ConfigurationManager : BaseUnityPlugin
+    {
+        /// <summary>
+        /// GUID of this plugin
+        /// </summary>
+        public const string GUID = "com.bepis.bepinex.configurationmanager";
+
+        /// <summary>
+        /// Version constant
+        /// </summary>
+        public const string Version = "18.0";
+
+        internal static new ManualLogSource Logger;
+        private static SettingFieldDrawer _fieldDrawer;
+
+        private static readonly Color _advancedSettingColor = new Color(1f, 0.95f, 0.67f, 1f);
+        private const int WindowId = -68;
+
+        private const string SearchBoxName = "searchBox";
+        private bool _focusSearchBox;
+        private string _searchString = string.Empty;
+
+        /// <summary>
+        /// Event fired every time the manager window is shown or hidden.
+        /// </summary>
+        public event EventHandler<ValueChangedEventArgs<bool>> DisplayingWindowChanged;
+
+        /// <summary>
+        /// Disable the hotkey check used by config manager. If enabled you have to set <see cref="DisplayingWindow"/> to show the manager.
+        /// </summary>
+        public bool OverrideHotkey;
+
+        private bool _displayingWindow;
+        private bool _obsoleteCursor;
+
+        private string _modsWithoutSettings;
