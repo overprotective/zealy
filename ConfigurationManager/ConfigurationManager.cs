f44e90c43105d6a5d265a9607e2710c78cd789ff
overprotective <105806513+overprotective@users.noreply.github.com>
2020-10-07 17:58:41 +0000

    Custom commit

--- ConfigurationManager/ConfigurationManager.cs
+++ ConfigurationManager/ConfigurationManager.cs
@@ -58,3 +58,61 @@ namespace ConfigurationManager
         private bool _obsoleteCursor;
 
         private string _modsWithoutSettings;
+
+        private List<SettingEntryBase> _allSettings;
+        private List<PluginSettingsData> _filteredSetings = new List<PluginSettingsData>();
+
+        internal Rect SettingWindowRect { get; private set; }
+        private bool _windowWasMoved;
+
+        private bool _tipsPluginHeaderWasClicked, _tipsWindowWasMoved;
+
+        private Rect _screenRect;
+        private Vector2 _settingWindowScrollPos;
+        private int _tipsHeight;
+
+        private PropertyInfo _curLockState;
+        private PropertyInfo _curVisible;
+        private int _previousCursorLockState;
+        private bool _previousCursorVisible;
+
+        internal static Texture2D TooltipBg { get; private set; }
+        internal static Texture2D WindowBackground { get; private set; }
+
+        internal int LeftColumnWidth { get; private set; }
+        internal int RightColumnWidth { get; private set; }
+
+        private readonly ConfigEntry<bool> _showAdvanced;
+        private readonly ConfigEntry<bool> _showKeybinds;
+        private readonly ConfigEntry<bool> _showSettings;
+        private readonly ConfigEntry<KeyboardShortcut> _keybind;
+        private readonly ConfigEntry<bool> _hideSingleSection;
+        private readonly ConfigEntry<bool> _pluginConfigCollapsedDefault;
+        private bool _showDebug;
+
+        /// <inheritdoc />
+        public ConfigurationManager()
+        {
+            Logger = base.Logger;
+            _fieldDrawer = new SettingFieldDrawer(this);
+
+            _showAdvanced = Config.Bind("Filtering", "Show advanced", false);
+            _showKeybinds = Config.Bind("Filtering", "Show keybinds", true);
+            _showSettings = Config.Bind("Filtering", "Show settings", true);
+            _keybind = Config.Bind("General", "Show config manager", new KeyboardShortcut(KeyCode.F1),
+                new ConfigDescription("The shortcut used to toggle the config manager window on and off.\n" +
+                                      "The key can be overridden by a game-specific plugin if necessary, in that case this setting is ignored."));
+            _hideSingleSection = Config.Bind("General", "Hide single sections", false, new ConfigDescription("Show section title for plugins with only one section"));
+            _pluginConfigCollapsedDefault = Config.Bind("General", "Plugin collapsed default", true, new ConfigDescription("If set to true plugins will be collapsed when opening the configuration manager window"));
+        }
+
+        /// <summary>
+        /// Is the config manager main window displayed on screen
+        /// </summary>
+        public bool DisplayingWindow
+        {
+            get => _displayingWindow;
+            set
+            {
+                if (_displayingWindow == value) return;
+                _displayingWindow = value;
