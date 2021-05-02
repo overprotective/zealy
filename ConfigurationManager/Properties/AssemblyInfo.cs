42ba01f6bfce09b80b8981b138f6dc2bc2ac7571
overprotective <105806513+overprotective@users.noreply.github.com>
2021-05-02 20:45:04 +0000

    updated Slovak translation

---
+++ ConfigurationManager/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyProduct("BepInEx.ConfigurationManager")]
+[assembly: AssemblyTitle("BepInEx.ConfigurationManager")]
+
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("https://github.com/BepInEx/BepInEx.ConfigurationManager")]
