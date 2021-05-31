179219cbac7ed07b1fe983d3ac9681d1c9fb7fb1
overprotective <105806513+overprotective@users.noreply.github.com>
2021-05-31 17:16:59 +0000

    Forward zendframework/zf2#3721

--- ConfigurationManager/SettingEntryBase.cs
+++ ConfigurationManager/SettingEntryBase.cs
@@ -54,3 +54,49 @@ namespace ConfigurationManager
         /// Show this setting in the settings screen at all? If false, don't show.
         /// </summary>
         public bool? Browsable { get; protected set; }
+
+        /// <summary>
+        /// Category the setting is under. Null to be directly under the plugin.
+        /// </summary>
+        public string Category { get; protected set; }
+
+        /// <summary>
+        /// If set, a "Default" button will be shown next to the setting to allow resetting to default.
+        /// </summary>
+        public object DefaultValue { get; protected set; }
+
+        /// <summary>
+        /// Force the "Reset" button to not be displayed, even if a valid DefaultValue is available. 
+        /// </summary>
+        public bool HideDefaultButton { get; protected set; }
+
+        /// <summary>
+        /// Force the setting name to not be displayed. Should only be used with a <see cref="CustomDrawer"/> to get more space.
+        /// Can be used together with <see cref="HideDefaultButton"/> to gain even more space.
+        /// </summary>
+        public bool HideSettingName { get; protected set; }
+
+        /// <summary>
+        /// Optional description shown when hovering over the setting
+        /// </summary>
+        public string Description { get; protected internal set; }
+
+        /// <summary>
+        /// Name of the setting
+        /// </summary>
+        public virtual string DispName { get; protected internal set; }
+
+        /// <summary>
+        /// Plugin this setting belongs to
+        /// </summary>
+        public BepInPlugin PluginInfo { get; protected internal set; }
+
+        /// <summary>
+        /// Only allow showing of the value. False whenever possible by default.
+        /// </summary>
+        public bool? ReadOnly { get; protected set; }
+
+        /// <summary>
+        /// Type of the variable
+        /// </summary>
+        public abstract Type SettingType { get; }
