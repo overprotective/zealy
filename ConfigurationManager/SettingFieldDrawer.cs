df64e89df0d66917721cfcc3f814432b1877d867
overprotective <105806513+overprotective@users.noreply.github.com>
2021-06-20 06:12:16 +0000

    msm: vidc: Change in input buffer size calculation

--- ConfigurationManager/SettingFieldDrawer.cs
+++ ConfigurationManager/SettingFieldDrawer.cs
@@ -6,4 +6,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Glob
+using System.Globalization;
+using System.Linq;
+using BepInEx;
+using 
