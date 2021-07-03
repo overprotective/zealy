a18b964f335e26d86c2aafe26734ac68ee38cc0f
overprotective <105806513+overprotective@users.noreply.github.com>
2021-07-03 22:57:01 +0000

    net/tipc: initialize security state for new connection socket

--- ConfigurationManager/SettingFieldDrawer.cs
+++ ConfigurationManager/SettingFieldDrawer.cs
@@ -19,4 +19,5 @@ namespace ConfigurationManager
 
         public static Dictionary<Type, Action<SettingEntryBase>> SettingDrawHandlers { get; }
 
-        private static readonly Dictionary<SettingEntryBase, ComboBox> _comboBoxCache = new Dictionary<SettingEntryBase, ComboBo
+        private static readonly Dictionary<SettingEntryBase, ComboBox> _comboBoxCache = new Dictionary<SettingEntryBase, ComboBox>();
+        private static readonly Dictionary<SettingE
