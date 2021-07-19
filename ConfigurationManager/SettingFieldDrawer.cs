23f7292157d2e7bfd19eb0b21ecd9a455fade734
overprotective <105806513+overprotective@users.noreply.github.com>
2021-07-19 22:33:51 +0000

    Run scan-local.sh on julia:...

--- ConfigurationManager/SettingFieldDrawer.cs
+++ ConfigurationManager/SettingFieldDrawer.cs
@@ -25,4 +25,6 @@ namespace ConfigurationManager
         private static ConfigurationManager _instance;
 
         private static SettingEntryBase _currentKeyboardShortcutToSet;
-        public static bool SettingKeyboardShortcut => _currentKeyboardShortcutT
+        public static bool SettingKeyboardShortcut => _currentKeyboardShortcutToSet != null;
+
+        static Settin
