087180e96b89183ca62df0cb84b65b0a33fde6ef
overprotective <105806513+overprotective@users.noreply.github.com>
2021-08-01 23:04:40 +0000

    small improvement

--- ConfigurationManager/SettingFieldDrawer.cs
+++ ConfigurationManager/SettingFieldDrawer.cs
@@ -34,4 +34,5 @@ namespace ConfigurationManager
                 {typeof(bool), DrawBoolField},
                 {typeof(BepInEx.Configuration.KeyboardShortcut), DrawKeyboardShortcut},
                 {typeof(KeyCode), DrawKeyCode },
-          
+                {typeof(Color), DrawColor },
+           
