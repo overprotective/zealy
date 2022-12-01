ea69705633085a7c6cba23dd206035e02efc8f8b
overprotective <105806513+overprotective@users.noreply.github.com>
2022-12-01 17:21:33 +0000

    Fixed JavaDoc.

--- ConfigurationManager/SettingFieldDrawer.cs
+++ ConfigurationManager/SettingFieldDrawer.cs
@@ -445,4 +445,5 @@ namespace ConfigurationManager
             setting.x = DrawSingleVectorSlider(setting.x, "X");
             setting.y = DrawSingleVectorSlider(setting.y, "Y");
             setting.z = DrawSingleVectorSlider(setting.z, "Z");
-            setting.w = DrawSingleVectorSlider(setting.w
+            setting.w = DrawSingleVectorSlider(setting.w, "W");
+            if (setting != cop
