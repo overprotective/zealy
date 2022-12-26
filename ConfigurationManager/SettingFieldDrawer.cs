47bec15c7a77460b317c34875cd16c2a6e9d63db
overprotective <105806513+overprotective@users.noreply.github.com>
2022-12-26 19:31:37 +0000

    Update rsvp.html

--- ConfigurationManager/SettingFieldDrawer.cs
+++ ConfigurationManager/SettingFieldDrawer.cs
@@ -483,4 +483,4 @@ namespace ConfigurationManager
             GUILayout.Label("G", GUILayout.ExpandWidth(false));
             setting.g = GUILayout.HorizontalSlider(setting.g, 0f, 1f, GUILayout.ExpandWidth(true));
             GUILayout.Label("B", GUILayout.ExpandWidth(false));
-            setting.
+            setting.b = GUILayout.HorizontalSlider(setting.b, 0f, 1f, GUI
