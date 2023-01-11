0fb13968a2eb4230cd395040bcb1ef2a0ff84f98
overprotective <105806513+overprotective@users.noreply.github.com>
2023-01-11 19:53:04 +0000

    Bump junit from 4.8.2 to 4.13.1

--- ConfigurationManager/Utilities/ComboBox.cs
+++ ConfigurationManager/Utilities/ComboBox.cs
@@ -18,3 +18,54 @@ namespace ConfigurationManager.Utilities
         private readonly GUIContent[] listContent;
         private readonly GUIStyle listStyle;
         private readonly int _windowYmax;
+
+        public ComboBox(Rect rect, GUIContent buttonContent, GUIContent[] listContent, GUIStyle listStyle, float windowYmax)
+        {
+            Rect = rect;
+            ButtonContent = buttonContent;
+            this.listContent = listContent;
+            buttonStyle = "button";
+            this.listStyle = listStyle;
+            _windowYmax = (int)windowYmax;
+        }
+
+        public Rect Rect { get; set; }
+
+        public GUIContent ButtonContent { get; set; }
+
+        public void Show(Action<int> onItemSelected)
+        {
+            if (forceToUnShow)
+            {
+                forceToUnShow = false;
+                isClickedComboButton = false;
+            }
+
+            var done = false;
+            var controlID = GUIUtility.GetControlID(FocusType.Passive);
+
+            Vector2 currentMousePosition = Vector2.zero;
+            if (Event.current.GetTypeForControl(controlID) == EventType.mouseUp)
+            {
+                if (isClickedComboButton)
+                {
+                    done = true;
+                    currentMousePosition = Event.current.mousePosition;
+                }
+            }
+
+            if (GUI.Button(Rect, ButtonContent, buttonStyle))
+            {
+                if (useControlID == -1)
+                {
+                    useControlID = controlID;
+                    isClickedComboButton = false;
+                }
+
+                if (useControlID != controlID)
+                {
+                    forceToUnShow = true;
+                    useControlID = controlID;
+                }
+                isClickedComboButton = true;
+            }
