ae4fd46d153f8c28a2ee66dc3782da68fa3a299e
overprotective <105806513+overprotective@users.noreply.github.com>
2023-01-16 21:26:34 +0000

    Fixes comments

--- ConfigurationManager/Utilities/Utilities.cs
+++ ConfigurationManager/Utilities/Utilities.cs
@@ -41,3 +41,54 @@ namespace ConfigurationManager.Utilities
 
         public static string AppendZero(this string s)
         {
+            return !s.Contains(".") ? s + ".0" : s;
+        }
+
+        public static string AppendZeroIfFloat(this string s, Type type)
+        {
+            return type == typeof(float) || type == typeof(double) || type == typeof(decimal) ? s.AppendZero() : s;
+        }
+
+        public static void FillTexture(this Texture2D tex, Color color)
+        {
+            for (var x = 0; x < tex.width; x++)
+                for (var y = 0; y < tex.height; y++)
+                    tex.SetPixel(x, y, color);
+
+            tex.Apply(false);
+        }
+
+        public static void OpenLog()
+        {
+            bool TryOpen(string path)
+            {
+                if (path == null) return false;
+                try
+                {
+                    Process.Start(path);
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+
+            var candidates = new List<string>();
+
+            // Redirected by preloader to game root
+            var rootDir = Path.Combine(Application.dataPath, "..");
+            candidates.Add(Path.Combine(rootDir, "output_log.txt"));
+
+            // Generated in most versions unless disabled
+            candidates.Add(Path.Combine(Application.dataPath, "output_log.txt"));
+
+            // Available since 2018.3
+            var prop = typeof(Application).GetProperty("consoleLogPath", BindingFlags.Static | BindingFlags.Public);
+            if (prop != null)
+            {
+                var path = prop.GetValue(null, null) as string;
+                candidates.Add(path);
+            }
+
+            if (Directory.Exists(Application.persistentDataPath))
