f25f17a5b2ff283175d84be97bbe9e5a65c72e61
overprotective <105806513+overprotective@users.noreply.github.com>
2023-02-22 17:10:59 +0000

    Bump to v1.1.1

--- ConfigurationManagerAttributes.cs
+++ ConfigurationManagerAttributes.cs
@@ -78,3 +78,67 @@ internal sealed class ConfigurationManagerAttributes
     /// <param name="setting">
     /// Setting currently being set (if available).
     /// </param>
+    /// <param name="isCurrentlyAcceptingInput">
+    /// Set this ref parameter to true when you want the current setting drawer to receive Input events.
+    /// The value will persist after being set, use it to see if the current instance is being edited.
+    /// Remember to set it to false after you are done!
+    /// </param>
+    public delegate void CustomHotkeyDrawerFunc(BepInEx.Configuration.ConfigEntryBase setting, ref bool isCurrentlyAcceptingInput);
+
+    /// <summary>
+    /// Show this setting in the settings screen at all? If false, don't show.
+    /// </summary>
+    public bool? Browsable;
+
+    /// <summary>
+    /// Category the setting is under. Null to be directly under the plugin.
+    /// </summary>
+    public string Category;
+
+    /// <summary>
+    /// If set, a "Default" button will be shown next to the setting to allow resetting to default.
+    /// </summary>
+    public object DefaultValue;
+
+    /// <summary>
+    /// Force the "Reset" button to not be displayed, even if a valid DefaultValue is available. 
+    /// </summary>
+    public bool? HideDefaultButton;
+
+    /// <summary>
+    /// Force the setting name to not be displayed. Should only be used with a <see cref="CustomDrawer"/> to get more space.
+    /// Can be used together with <see cref="HideDefaultButton"/> to gain even more space.
+    /// </summary>
+    public bool? HideSettingName;
+
+    /// <summary>
+    /// Optional description shown when hovering over the setting.
+    /// Not recommended, provide the description when creating the setting instead.
+    /// </summary>
+    public string Description;
+
+    /// <summary>
+    /// Name of the setting.
+    /// </summary>
+    public string DispName;
+
+    /// <summary>
+    /// Order of the setting on the settings list relative to other settings in a category.
+    /// 0 by default, higher number is higher on the list.
+    /// </summary>
+    public int? Order;
+
+    /// <summary>
+    /// Only show the value, don't allow editing it.
+    /// </summary>
+    public bool? ReadOnly;
+
+    /// <summary>
+    /// If true, don't show the setting by default. User has to turn on showing advanced settings or search for it.
+    /// </summary>
+    public bool? IsAdvanced;
+
+    /// <summary>
+    /// Custom converter from setting type to string for the built-in editor textboxes.
+    /// </summary>
+    public System.Func<object, string> ObjToStr;
